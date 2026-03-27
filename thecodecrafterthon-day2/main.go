85f37261760b21e06a3a1514417e18c3ce5e6333
AshomAzi <ashomazibulus@gmail.com>
2026-03-27 17:00:54 +0100

    Hexadecimal Operations

---
+++ thecodecrafterthon-day2/main.go
@@ -0,0 +1,30 @@
+package main
+
+import "fmt"
+
+var value string
+
+func hexToDec() {
+
+}
+
+func binToDec() {
+
+}
+
+func decToBin() {
+
+}
+
+func hexToBin() {
+	
+}
+
+func main() {
+
+	fmt.Println("Welcome to the Base Converter:")
+	fmt.Println("Choose an operation\n1. Hexadecimal to Binary\n2. Binary to Decimal\n3. Decimal to Hexadecimal\n4. Decimal to Binary")
+	fmt.Print("Input a value: ")
+	fmt.Scan(&value)
+
+}
