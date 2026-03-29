43f0923f600be2531dbc2b007fd53aefc8f64f77
AshomAzi <ashomazibulus@gmail.com>
2026-03-29 18:45:29 +0100

    Base Converter

--- thecodecrafterthon-day2/main.go
+++ thecodecrafterthon-day2/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"strconv"
+	"strings"
 )
 
 var value string
@@ -10,7 +11,7 @@ var operation string
 
 func hexToDec() {
 	fmt.Print("Input a value: ")
-	start1:
+start1:
 	fmt.Scanln(&value)
 	val1, err := strconv.ParseInt(value, 16, 64)
 	if err == nil {
@@ -23,7 +24,7 @@ func hexToDec() {
 
 func binToDec() {
 	fmt.Print("Input a binary value: ")
-	start1:
+start1:
 	fmt.Scanln(&value)
 	val1, err := strconv.ParseInt(value, 2, 64)
 	if err == nil {
@@ -35,16 +36,26 @@ func binToDec() {
 }
 
 func hexbinToDec() {
-
+	fmt.Print("Input a valid decimal value: ")
+	start1:
+	fmt.Scan(&value)
+	val1, err := strconv.Atoi(value)
+	if err == nil {
+		res1 := strconv.FormatInt(int64(val1), 2)
+		res2 := strconv.FormatInt(int64(val1), 16)
+		fmt.Printf("Binary value of %v is %v\nHexadecimal value of %v is %v\n", value, res1, value, strings.ToUpper(res2))
+	} else {
+		fmt.Print("Input a valid decimal value: ")
+		goto start1
+	}
 }
 
 func main() {
 	fmt.Println("Welcome to the Base Converter:")
-	start1:
+start1:
 	fmt.Println("Choose an operation\n1. To convert from Hexadecimal to Binary\n2. To convert from Binary to Decimal\n3. To convert from Decimal Hexadecimal and Binary.")
 	fmt.Print(">>> ")
 	fmt.Scan(&operation)
-	fmt.Println()
 
 	if operation == "1" {
 		hexToDec()
@@ -54,10 +65,11 @@ func main() {
 		goto start1
 	} else if operation == "3" {
 		hexbinToDec()
-	} else if operation == "" {
-		fmt.Print("Empty input is not supported")
+		goto start1
+	// } else if operation  {
+	// 	fmt.Print("Empty input is not supported")
 	} else {
-		fmt.Println("Invalid Operation, Try Again: ")
+		fmt.Print("Invalid Operation, Try Again: ")
 		goto start1
 	}
 
