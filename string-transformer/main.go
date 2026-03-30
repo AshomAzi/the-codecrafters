e3c729662b17330396a80757a3e4c067f5d99aa3
AshomAzi <ashomazibulus@gmail.com>
2026-03-30 12:02:11 +0100

    string transformers

---
+++ string-transformer/main.go
@@ -0,0 +1,47 @@
+// CodeCrafters — Operation Gopher Protocol
+// Module: String Transformer
+// Author: [Azi Bulus]
+// Squad:  [Pointers]
+
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+
+var text string
+var command string
+
+func upper() {
+	fmt.Println("Input a value! ")
+	file := bufio.NewScanner(os.Stdin)
+	fmt.Println(file)
+}
+
+func lower() {
+
+}
+
+func caps() {
+
+}
+
+func title() {
+
+}
+
+func snake_case() {
+
+}
+
+func reverse() {
+
+}
+
+func main() {
+	upper()
+}
+
